09afa01be6b560c94eea13035649ebf5569949dc
Sebastian Puczynski <sebastian_puczynski@SebastinskisMBP.frontier.com>
2020-01-06 22:14:34 -0500

    Fixed some formatting.

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,34 @@
 // Stack --------------------------------------------------------
-
 class Stack {
-	constructor() {
-		this.items = []
-	}
+  constructor() {
+    this.items = [];
+  }
 
-	pop() {
-		if (this.items.length === 0) {
-			return Null
-		}
-		else {
-			return this.items.pop();
-		}
-	}
+  pop() {
+    if (this.items.length === 0) {
+      return Null;
+    } else {
+      return this.items.pop();
+    }
+  }
 
-	push(number) {
-		// When an operation is click, the number will be pushed onto the stack!		
-		this.items.push(number);
-	}
+  push(number) {
+    // When an operation is click, the number will be pushed onto the stack!
+    this.items.push(number);
+  }
 
-	peek() {
-		return this.items[this.items.length - 1];
-	}
+  peek() {
+    return this.items[this.items.length - 1];
+  }
 
-	showStack() {
-		return this.items
-	}
+  showStack() {
+    return this.items;
+  }
 
-	reset() {
-		this.items = []
-	}
-
+  reset() {
+    this.items = [];
+  }
 }
-
 // INIT -----------------------------------------------------------------
 var numStack = new Stack();
 var opStack = new Stack();
